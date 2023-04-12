9f0c1ad62756cf67b8f4fb433ef4610cac070cdf
harusnoopy <117557776+harusnoopy@users.noreply.github.com>
2023-04-12 17:44:04 +0000

    create organizer's index

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -4,11 +4,30 @@ class Admin::EventsController < ApplicationController
   end
   
   def new
+    @event = Event.new
+  end
+  
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to admin_event_path(@event.id)
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+  end
+  
+  def update
+  end
+  
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :introduction, :date, :schedule, :number_of_people, :is_recruiting)
   end
 end
--- app/controllers/admin/organizers_controller.rb
+++ app/controllers/admin/organizers_controller.rb
@@ -1,13 +1,33 @@
 class Admin::OrganizersController < ApplicationController
   def index
+    @organizers = Organizer.page(params[:page])
   end
 
   def new
+    @organizer = Organizer.new
+  end
+
+  def create
+    @organizer = Organizer.new(organizer_params)
+    if @organizer.save
+      redirect_to admin_organizer_path(@organizer.id)
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def organizer_params
+    params.require(:organizer).permit(:name, :introduction, :postal_code, :address, :telephone_number, :email, :homepage)
   end
 end
