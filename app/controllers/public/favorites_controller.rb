6b1dffa3656fa2b70bc53f5c1cde53bd49da3e62
harusnoopy <117557776+harusnoopy@users.noreply.github.com>
2023-04-16 17:06:37 +0000

    create index of favorite

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,16 +3,17 @@ class Public::FavoritesController < ApplicationController
   def create
     favorite = Favorite.new(user_id: current_user.id, event_id: params[:event_id])
     favorite.save
-    redirect_to events_path
+    redirect_to request.referer
   end
 
   def destroy
     favorite = Favorite.find_by(user_id: current_user.id, event_id: params[:event_id])
     favorite.destroy
-    redirect_to events_path
+    redirect_to request.referer
   end
 
   def index
+    @favorites = current_user.favorites.page(params[:page])
   end
 
 end
--- app/controllers/public/participations_controller.rb
+++ app/controllers/public/participations_controller.rb
@@ -1,8 +1,20 @@
 class Public::ParticipationsController < ApplicationController
   def new
+    @participation = Participation.new
   end
 
   def confirmation
+    @participation = Participation.new(participation_params)
+  end
+  
+  def create
+    participation = Participation.new(participation_params)
+    participation.user_id = current_user.id
+    if participation.save
+      redirect_to thanx_path
+    else
+      render :confirmation
+    end
   end
 
   def thanx
@@ -13,4 +25,11 @@ class Public::ParticipationsController < ApplicationController
 
   def show
   end
+  
+  private
+  
+  def participation_params
+    params.require(:participation).permit(:user_id, :event_id, :is_applying)
+  end
+  
 end
