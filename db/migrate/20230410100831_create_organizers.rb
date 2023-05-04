b557cc3142acd5bd06cbc684eab6aec36ef56350
harusnoopy <117557776+harusnoopy@users.noreply.github.com>
2023-05-04 07:50:24 +0000

    fix db

--- db/migrate/20230410100831_create_organizers.rb
+++ db/migrate/20230410100831_create_organizers.rb
@@ -1,7 +1,7 @@
 class CreateOrganizers < ActiveRecord::Migration[6.1]
   def change
     create_table :organizers do |t|
-      
+
       t.string :name, null: false
       t.text :introduction, null: false
       t.string :postal_code, null: false
