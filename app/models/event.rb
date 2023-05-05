87df5ed07db79dbdeb9f82cff10231f3ce8622d3
harusnoopy <117557776+harusnoopy@users.noreply.github.com>
2023-05-05 07:08:23 +0000

    add validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,14 @@ class Event < ApplicationRecord
   has_many :participation_users, through: :participations, source: :user
   has_many :favorites, dependent: :destroy
   belongs_to :organizer
-  
+
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :date, presence: true
+  validates :schedule, presence: true
+  validates :number_of_people, presence: true
+  validates :organizer_id, presence: true
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -1,3 +1,12 @@
 class Organizer < ApplicationRecord
   has_many :events, dependent: :destroy
+
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :telephone_number, presence: true
+  validates :email, presence: true
+  validates :homepage, presence: true
+
 end
