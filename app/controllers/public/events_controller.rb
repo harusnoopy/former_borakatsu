74a5f57adf7e64c71456afbda1ee764c93242b87
harusnoopy <117557776+harusnoopy@users.noreply.github.com>
2023-05-21 08:23:21 +0000

    fix of application cancellation function

--- app/controllers/public/events_controller.rb
+++ app/controllers/public/events_controller.rb
@@ -5,6 +5,9 @@ class Public::EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+    if user_signed_in?
+      @event_participation = Participation.find_by(event_id: params[:id], user_id: current_user.id)
+    end
     @participation = Participation.new
   end
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -4,10 +4,11 @@ class Public::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to user_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to user_path, notice: 'ゲストユーザーとしてログインしました'
   end  
   
   def after_sign_in_path_for(resource)
+    flash[:notice] = "ログインしました"
     user_path
   end
   
