a0ba137fd4f9d7a40bd9dae08790bdac7f309a27
harusnoopy <117557776+harusnoopy@users.noreply.github.com>
2023-10-10 08:20:52 +0000

    asynchronization of favorites

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,16 +1,16 @@
 class Public::FavoritesController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
+    @event = Event.find(params[:event_id])
     favorite = Favorite.new(user_id: current_user.id, event_id: params[:event_id])
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
+    @event = Event.find(params[:event_id])
     favorite = Favorite.find_by(user_id: current_user.id, event_id: params[:event_id])
     favorite.destroy
-    redirect_to request.referer
   end
 
   def index
